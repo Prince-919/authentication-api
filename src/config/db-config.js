8c457c7d05eb006846f8a2d854b1dc446e3f9512
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2024-11-25 14:48:23 +0530

    add database connection successfully!

---
+++ src/config/db-config.js
@@ -0,0 +1,18 @@
+const { connection, connect } = require("mongoose");
+const config = require("./config");
+
+const dbConnect = async () => {
+  try {
+    connection.on("connected", () => {
+      console.log("Connected to database successfully.");
+    });
+    connection.on("error", (err) => {
+      console.log(`Error in connecting to database: ${err.message}`);
+    });
+    await connect(config.get("databaseUrl"));
+  } catch (err) {
+    console.log(`failed to connect to database: ${err.message}`);
+    process.exit(1);
+  }
+};
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const config = require("./config/config");
+const dbConnect = require("./config/db-config");
 
 const app = express();
 
 const serverStart = async () => {
   try {
+    await dbConnect();
     const port = config.get("port") || 8000;
     app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
+      console.log(`Server is running at http://localhost:${port}.`);
     });
   } catch (error) {
     console.error("Error starting the server:", error);
